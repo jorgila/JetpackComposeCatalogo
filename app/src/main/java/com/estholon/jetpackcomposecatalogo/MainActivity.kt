2acffb8db64d456d933819415fd69bcfcf0ec54e
Jorge Gimeno <jgimenolatas@gmail.com>
2024-03-08 20:14:57 +0100

    "Text and TextField"

--- app/src/main/java/com/estholon/jetpackcomposecatalogo/MainActivity.kt
+++ app/src/main/java/com/estholon/jetpackcomposecatalogo/MainActivity.kt
@@ -11,12 +11,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -29,6 +34,11 @@ import androidx.compose.ui.unit.dp
 import com.estholon.jetpackcomposecatalogo.ui.theme.JetpackComposeCatalogoTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,7 +50,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MyStateExample()
+                    MyOutlinedTextField()
                 }
             }
         }
@@ -96,10 +106,77 @@ fun MyBox(name: String, modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun MyText(){
+    Column(Modifier.fillMaxSize()) {
+        Text(text = "Esto es un ejemplo")
+        Text(text = "Esto es un ejemplo", color = Color.Blue)
+        Text(text = "Esto es un ejemplo", fontWeight = FontWeight.ExtraBold)
+        Text(text = "Esto es un ejemplo", style = TextStyle(fontFamily = FontFamily.Cursive))
+        Text(text = "Esto es un ejemplo", style = TextStyle(textDecoration = TextDecoration.LineThrough))
+        Text(text = "Esto es un ejemplo", style = TextStyle(textDecoration = TextDecoration.Underline))
+        Text(text = "Esto es un ejemplo", style = TextStyle(textDecoration = TextDecoration.combine(
+            listOf(TextDecoration.LineThrough, TextDecoration.Underline)
+        )))
+        Text(text = "Esto es una prueba", fontSize = 30.sp)
+    }
+}
+
+@Composable
+fun MyTextField(){
+
+    Column(Modifier.fillMaxSize()) {
+        var myText by rememberSaveable { mutableStateOf("") }
+        TextField(value = myText, onValueChange = {myText = it}, modifier = Modifier.fillMaxWidth())
+    }
+}
+
+
+@Composable
+fun MyTextFieldAdvance(){
+
+    Column(Modifier.fillMaxSize()) {
+        var myText by rememberSaveable { mutableStateOf("") }
+        TextField(
+            value = myText,
+            onValueChange = {myText =
+                if(it.contains("@")){
+                    it.replace("@","arroba")
+                    } else {
+                    it
+                    }
+                            },
+            label = { Text(text="Introduce tu nombre") },
+            modifier = Modifier.fillMaxWidth())
+    }
+}
+
+@Composable
+fun MyOutlinedTextField(){
+    Column(Modifier.fillMaxSize()) {
+        var myText by rememberSaveable { mutableStateOf("") }
+        OutlinedTextField(
+            value = myText,
+            onValueChange = { myText = it },
+            label = { Text(text = "Introduce tu nombre") },
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Blue,
+                unfocusedIndicatorColor = Color.Red,
+                unfocusedContainerColor = Color.Transparent,
+                focusedContainerColor = Color.Transparent,
+                focusedLabelColor = Color.Green
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackComposeCatalogoTheme {
-        MyStateExample()
+        MyOutlinedTextField()
     }
 }
